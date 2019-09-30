7e3ec126ff7da6e5a978b4be9e1704e328ca6fa0
Lucas Henrique de Assis Monteiro <51081529+lucashenriquedeassismonteiro@users.noreply.github.com>
2019-09-30 13:05:42 -0300

    Add files via upload

---
+++ Maratona/src/Maratona/Time.java
@@ -0,0 +1,14 @@
+package Maratona;
+
+public class Time extends Jogo{
+	public String nome;
+	public int pontos;
+	public int fatortecnica;
+	
+	public Time (String nome, int pontos, int fatortecnica) {
+		this.nome = nome;
+		this.pontos = pontos;
+		this.fatortecnica = fatortecnica;
+	}
+	
+}
